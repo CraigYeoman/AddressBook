56f6d5d97170e7ed6cbc1221a26f9e51358b8dcb
Craig Yeoman <yeomancraig@outlook.com>
2024-01-25 06:22:17 -0600

    Added Postrgres Database, updating User Model, extending Identity and Registration

--- AddressBook/Data/ApplicationDbContext.cs
+++ AddressBook/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AddressBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AddressBook.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
     }
 }
