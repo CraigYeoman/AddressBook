088ebc3ef3389cd0a7d7598f45b99c6a867d3a1d
Craig Yeoman <yeomancraig@outlook.com>
2024-01-28 11:19:57 -0600

    Category FIlter for Contacts - Contacts Controller - Index(), Update User Schema

--- AddressBook/Models/AppUser.cs
+++ AddressBook/Models/AppUser.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
@@ -14,9 +16,13 @@ namespace AddressBook.Models
         [Required]
         [Display(Name = "Last Name")]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max of {1} characters long.", MinimumLength = 2)]
-        public string? LastName { get; set;}
+        public string? LastName { get; set; }
 
         [NotMapped]
         public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
     }
 }
