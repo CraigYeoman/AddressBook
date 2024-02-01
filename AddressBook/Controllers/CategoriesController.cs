b17907616fb58efb6ad60e227b272e027de33347
Craig Yeoman <yeomancraig@outlook.com>
2024-02-01 06:30:11 -0600

    Create Category View and Controller

--- AddressBook/Controllers/CategoriesController.cs
+++ AddressBook/Controllers/CategoriesController.cs
@@ -61,7 +61,6 @@ namespace AddressBook.Controllers
         [Authorize]
         public IActionResult Create()
         {
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
 
@@ -73,13 +72,18 @@ namespace AddressBook.Controllers
         [Authorize]
         public async Task<IActionResult> Create([Bind("Id,AppUserId,Name")] Category category)
         {
+            ModelState.Remove("AppUserId");
+
+
             if (ModelState.IsValid)
             {
+                string appUserId = _userManager.GetUserId(User);
+                category.AppUserId = appUserId;
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
+
             return View(category);
         }
 
