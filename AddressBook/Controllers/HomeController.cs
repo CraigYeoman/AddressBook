f7cb3bafd7349056753e7173d5117cdc3a8d368e
Craig Yeoman <yeomancraig@outlook.com>
2024-02-03 10:15:26 -0600

    Error Page

--- AddressBook/Controllers/HomeController.cs
+++ AddressBook/Controllers/HomeController.cs
@@ -23,6 +23,24 @@ namespace AddressBook.Controllers
             return View();
         }
 
+        [Route("/Home/HandleError/{code:int}")]
+        public IActionResult HandlerError(int code)
+        {
+            var customError = new CustomError();
+            customError.code = code;
+
+            if (code == 404)
+            {
+                customError.message = "The page you are looking for is not found.";
+            }
+            else
+            {
+                customError.message = "Sorry, something when wrong."; 
+            }
+
+            return View("~/Views/Shared/CustomError.cshtml", customError);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
